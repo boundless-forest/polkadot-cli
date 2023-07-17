7704f5e616edbb96e5677cdac5f02f111a1c3c33
bear <boundless.forest@outlook.com>
2023-07-17 11:29:29 +0800

    Add multiple rpc clients

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ mod handler;
 mod networks;
 mod rpc;
 
+use app::Network;
 // crates.io
 use clap::Parser;
 use colored::Colorize;
+use networks::{ChainInfo, CrabChain, DarwiniaChain, PangoroChain};
 use rpc::RpcClient;
-use rustyline::error::ReadlineError;
+use rustyline::{error::ReadlineError, hint::HistoryHinter, history::FileHistory, Editor};
 // this crate
 use crate::{
-	app::{load_history, print_info, this_crate_editor, AppCommand},
+	app::{load_history, print_info, this_crate_editor, AppCommand, CommandHelper},
 	errors::{AppError, RpcError},
 	networks::{NoteTemplate, PangolinChain},
 };
@@ -24,12 +26,49 @@ async fn main() -> Result<(), AppError> {
 
 	// TODO: fix the unwrap()
 	let config = editor.helper().unwrap().config();
-	let rpc_client = RpcClient::<PangolinChain>::new(config).await?;
+	match config.network {
+		Network::Local => {
+			let rpc_client = RpcClient::<NoteTemplate>::new(config).await?;
+			run(&mut editor, &rpc_client).await?
+		},
+		Network::Polkadot => {
+			// let rpc_client = RpcClient::<>::new(config).await?;
+			todo!();
+		},
+		Network::Kusama => {
+			// let rpc_client = RpcClient::<NoteTemplate>::new(config).await?;
+			todo!();
+		},
+		Network::Crab => {
+			let rpc_client = RpcClient::<CrabChain>::new(config).await?;
+			run(&mut editor, &rpc_client).await?
+		},
+		Network::Darwinia => {
+			let rpc_client = RpcClient::<DarwiniaChain>::new(config).await?;
+			run(&mut editor, &rpc_client).await?
+		},
+		Network::Pangolin => {
+			let rpc_client = RpcClient::<PangolinChain>::new(config).await?;
+			run(&mut editor, &rpc_client).await?
+		},
+		Network::Pangoro => {
+			let rpc_client = RpcClient::<PangoroChain>::new(config).await?;
+			run(&mut editor, &rpc_client).await?
+		},
+	}
 
+	editor.save_history(&history_file)?;
+	Ok(())
+}
+
+pub async fn run<CI: ChainInfo>(
+	editor: &mut Editor<CommandHelper<HistoryHinter>, FileHistory>,
+	rpc_client: &RpcClient<CI>,
+) -> Result<(), AppError> {
 	print_info();
 	loop {
-		// TODO: Add the network tip before >>
-		let prompt = editor.readline(&"suber >> ".green().bold().to_string());
+		let command_tip = format!("suber ({:?}) >> ", <CI as ChainInfo>::NET_WORK);
+		let prompt = editor.readline(&command_tip.bright_yellow().italic().bold().to_string());
 		match prompt {
 			Ok(prompt) => {
 				if let Ok(command) = AppCommand::try_parse_from(prompt.split_whitespace()) {
@@ -48,7 +87,5 @@ async fn main() -> Result<(), AppError> {
 			},
 		}
 	}
-	editor.save_history(&history_file)?;
-
 	Ok(())
 }
